7f36973109fc439a24c96fe78ab9b96a46c09efb
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2016-11-30 09:41:41 -0500

    Add simple unit test

---
+++ DragonBidsServer/src/dragonbids/structures/listings/test/AuctionTest.groovy
@@ -0,0 +1,22 @@
+package dragonbids.structures.listings.test
+
+/**
+ * Created by amir on 11/30/16.
+ */
+class AuctionTest extends groovy.util.GroovyTestCase {
+    void testAddBid() {
+
+    }
+
+    void testGetHighBid() {
+
+    }
+
+    void testGetStartingBid() {
+
+    }
+
+    void testGetBidHistory() {
+
+    }
+}
